d538bdf626f80c92fdfbaf31ea0f22889edb0dbe
github username <anna.watt.lux@gmail.com>
2020-06-21 19:14:16 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "male", "brown", "rowdy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "male", "black", "rowdy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "female", "black", "friendly", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "female", "brown", "reserved", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "female", "black", "reserved", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "male", "black", "grumpy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "female", "black", "vocal", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "female", "brown", "violent", true);
